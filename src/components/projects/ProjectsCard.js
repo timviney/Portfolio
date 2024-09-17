44894d33a68d0b513bff46603e0e584ddaf76141
Tim Viney <t.c.viney@gmail.com>
2024-09-17 22:13:01 +0100

    set up a sudoku page

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -6,7 +6,9 @@ const ProjectsCard = ({ title, des, src }) => {
   return (
     <div 
       className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000"
-      onClick={click}>
+      // todo
+      //onClick={}
+      >
       <div className="w-full h-[80%] overflow-hidden rounded-lg">
         <img
           className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
@@ -37,44 +39,5 @@ const ProjectsCard = ({ title, des, src }) => {
     </div>
   );
 }
-
-const click = () =>{
-  sendMatrix(matrix)
-}
-
-async function sendMatrix(matrix) {
-  const response = await fetch('https://<api-id>.execute-api.<region>.amazonaws.com/<stage>/<resource>', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ matrix: matrix })
-  });
-
-  if (!response.ok) {
-    const message = `An error has occurred: ${response.statusText}`;
-    throw new Error(message);
-  }
-
-  const data = await response.json();
-  console.log(data);
-}
-
-// Example matrix
-const matrix = [
-  [2,0,9,0,0,0,6,0,0],
-  [0,4,0,8,7,0,0,1,2],
-  [8,0,0,0,1,9,0,4,0],
-  [0,3,0,7,0,0,8,0,1],
-  [0,6,5,0,0,8,0,3,0],
-  [1,0,0,0,3,0,0,0,7],
-  [0,0,0,6,5,0,7,0,9],
-  [6,0,4,0,0,0,0,2,0],
-  [0,8,0,3,0,1,4,5,0]
-];
-
-sendMatrix(matrix).catch(error => {
-  console.error('Error:', error);
-});
 
 export default ProjectsCard
