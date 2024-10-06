4e33aeec7a11aab31e03679c232b2ff61db3b7f1
Tim Viney <t.c.viney@gmail.com>
2024-10-06 17:48:10 +0100

    add random api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 
 const sudokuApi = process.env.REACT_APP_SUDOKU_URL;
+const dataAccessApi = process.env.REACT_APP_DATAACCESS_URL;
 
 export const solveMatrix = async (grid) => {
     const post = {
@@ -14,5 +15,34 @@ export const solveMatrix = async (grid) => {
         body: JSON.stringify(post),
     });
 
-    return response;
+    if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        throw new Error(message);
+    }
+
+    return await response.json();
+};
+
+export const randomSudoku = async () => {
+    const post = {
+        Table: 1,
+        Method: "Random",
+        RequestParameters: {
+            Difficulty: 1
+        }
+    };
+    const response = await fetch(dataAccessApi, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(post),
+    });
+    
+    if (!response.ok) {
+        const message = `An error has occurred: ${response.statusText}`;
+        throw new Error(message);
+    }
+
+    return await response.json();
 };
