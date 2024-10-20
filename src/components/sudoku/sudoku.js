8b6c041d01856bdf54b36ce97882d41bde4a95f9
Tim Viney <t.c.viney@gmail.com>
2024-10-20 20:59:20 +0100

    Hooking up random sudokus to a DB

--- src/components/sudoku/sudoku.js
+++ src/components/sudoku/sudoku.js
@@ -171,6 +171,7 @@ const SudokuSolver = () => {
           onClick={randomGrid}
           className="mt-8 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 w-24 flex items-center justify-center"
           disabled={isLoading}
+          title="AWS is expensive - sometimes the first call is slow :)"
         >
           Random
         </button>
