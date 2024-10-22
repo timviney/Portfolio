c1be71e5a6cd50e177d26aceb2c82e07233c9020
Tim Viney <t.c.viney@gmail.com>
2024-10-22 21:32:28 +0100

    Fix project links

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,15 @@
 import logo from "./images/logo.png";
 import me from "./images/me.png";
-import projectOne from "./images/projects/projectOne.jpg";
-import projectTwo from "./images/projects/projectTwo.jpg";
+import projectSudoku from "./images/projects/sudoku.png";
+import projectDataAccess from "./images/projects/dataaccess.png";
 import projectThree from "./images/projects/projectThree.jpeg";
 import contactImg from "./images/contact/contactImg.png";
 
 export {
   me,
   logo,
-  projectOne,
-  projectTwo,
+  projectSudoku,
+  projectDataAccess,
   projectThree,
   contactImg,
 };
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectTwo } from "../../assets/index";
+import { projectSudoku, projectDataAccess } from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
 
 const Projects = () => {
@@ -17,15 +17,17 @@ const Projects = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
-          title="TODO - C# Project"
-          des="Maybe some sort of poker calculator, or financial app, or money splitter,
-            google maps api that finds the flattest route for cycling?"
-          src={projectOne}
+          title="Sudoku Solver"
+          des="Solving sudoku problems via linear optimisation. Fill your own or a select a random one from the database."
+          src={projectSudoku}
+          sitePage={"sudoku"}
+          githubRepo={"https://github.com/timviney/Sudoku"}
         />
         <ProjectsCard
-          title="TODO - optimisation"
-          des="Maybe a suduko solver in Python?"
-          src={projectTwo}
+          title="Database Access"
+          des="Lambda function to sort database access for the site."
+          src={projectDataAccess}
+          githubRepo={"https://github.com/timviney/DataAccess"}
         />
       </div>
     </section>
