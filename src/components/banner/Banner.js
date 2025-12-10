197eab298147f2c39bc24678e7e7cec7b97b4222
Tim Viney <t.c.viney@gmail.com>
2025-12-10 16:25:57 +0000

    add my face

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import LeftBanner from './LeftBanner';
+import RightBanner from './RightBanner';
 const Banner = () => {
   return (
     <section
       id="home"
-      className="w-full min-h-screen pt-10 pb-20 flex flex-col justify-center border-b-[1px] font-titleFont border-b-black"
+      className="w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
     >
       <LeftBanner />
+      <RightBanner />
     </section>
   );
 }
